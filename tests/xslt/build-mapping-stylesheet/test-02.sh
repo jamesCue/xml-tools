35c652a2d0bcee62f49a56542a22eb1e9c177129
Nic Gibson <nicg@corbas.net>
2013-02-25 10:56:14 +0000

    Fixes to tests
    Corrected test-02 script and removed output files from tracking.
    
    Signed-off-by: Nic Gibson <nicg@corbas.net>

--- tests/xslt/build-mapping-stylesheet/test-02.sh
+++ tests/xslt/build-mapping-stylesheet/test-02.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-saxon ../../../msword/xslt/build-mapping-stylesheet.xsl test-01-map.xml test-01.xsl
-saxon test-01.xsl test-01.xml result-01.xml
+saxon ../../../msword/xslt/build-mapping-stylesheet.xsl test-02-map.xml test-02.xsl
